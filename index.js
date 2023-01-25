8293615019d6ba895ee43e2aff84bb4a7c2bc927
drake bishop <drakemx85@gmail.com>
2023-01-25 12:02:24 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+const textNode = document.createTextNode("grieve is the champion");
+newHeader.appendChild(textNode);
